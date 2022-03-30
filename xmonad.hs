1515af5e48365a9278c761a86563c64b8040ffca
Aaron Johnson <aaron@aaronjohnson.xyz>
2022-03-30 12:55:32 -0400

    changed color and size of window borders

--- xmonad.hs
+++ xmonad.hs
@@ -37,7 +37,7 @@ myClickJustFocuses = False
 
 -- Width of the window border in pixels.
 --
-myBorderWidth   = 1
+myBorderWidth   = 2
 
 -- modMask lets you specify which modkey you want to use. The default
 -- is mod1Mask ("left alt").  You may also consider using mod3Mask
@@ -59,8 +59,8 @@ myWorkspaces    = ["1","2","3","4","5","6","7","8","9"]
 
 -- Border colors for unfocused and focused windows, respectively.
 --
-myNormalBorderColor  = "#dddddd"
-myFocusedBorderColor = "#ff0000"
+myNormalBorderColor  = "#839496"
+myFocusedBorderColor = "#dc322f"
 
 ------------------------------------------------------------------------
 -- Key bindings. Add, modify or remove key bindings here.
